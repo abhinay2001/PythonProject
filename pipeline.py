830b5b00a2d2dc989070eb5daec0e729c0378032
Abhinay Goud Adhire <abhinay.adhire@gmail.com>
2026-01-03 10:56:01 -0800

    refactor: add reusable pipeline entrypoint for Airflow

---
+++ pipeline.py
@@ -0,0 +1,32 @@
+from logger import get_logger
+from util import get_tables, load_db_details
+from read import read_table
+from write import load_table
+from dq import validate_counts
+
+log = get_logger(__name__)
+
+
+def run_pipeline(env: str = "dev") -> None:
+    db_details = load_db_details(env)
+
+    source_db = db_details["SOURCE_DB"]
+    log.info(
+        "SOURCE_DB: type=%s host=%s port=%s user=%s name=%s",
+        source_db.get("DB_TYPE"),
+        source_db.get("DB_HOST"),
+        source_db.get("DB_PORT"),
+        source_db.get("DB_USER"),
+        source_db.get("DB_NAME"),
+    )
+
+    tables = get_tables("table_list")
+    for table_name in tables["table_name"]:
+        log.info("Reading data for %s", table_name)
+        data, column_names = read_table(db_details, table_name)
+
+        log.info("Columns for %s: %s", table_name, column_names)
+        log.info("Loading data for %s", table_name)
+        load_table(db_details, data, column_names, table_name)
+
+        validate_counts(db_details, table_name)
